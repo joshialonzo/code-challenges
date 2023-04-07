b98494029cdb0f0ae67e85a231e6e8198ff5d9e9
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-04-06 18:25:14 -0600

    Strings: Username validation (edge case)

--- code_challenges/strings/username_validation.py
+++ code_challenges/strings/username_validation.py
@@ -1,7 +1,23 @@
+"""
+Have the function username_validation(str) take the str parameter
+being passed and determine if the string is a valid username according to
+the following rules:
+
+1. The username is between 4 and 25 characters.
+2. It must start with a letter.
+3. It can only contain letters, numbers, and the underscore character.
+4. It cannot end with an underscore character.
+
+If the username is valid then your program should return the string true,
+otherwise return the string false.
+"""
+
+
 def username_validation(string):
 
   # code goes here
   username = string
+  between_4_and_25 = 4 <= len(username) <= 25
   letters = {letter for letter in username}
   start_with_a_letter = username[0].isalpha()
   has_valid_characters = all([
@@ -11,6 +27,7 @@ def username_validation(string):
   cannot_end_with_underscore = username[-1] != "_"
 
   if (
+    between_4_and_25 and
     start_with_a_letter and
     has_valid_characters and
     cannot_end_with_underscore
--- tests/strings/test_username_validation.py
+++ tests/strings/test_username_validation.py
@@ -1,11 +1,20 @@
 from code_challenges.strings.username_validation import username_validation
 
 
-def test_strings_username_validation_falsy():
+def test_strings_username_validation_truthy():
+    username = "u__hello_world123"
+    assert username_validation(username)
+
+def test_strings_username_validation_falsy_1():
     username = "aa_"
     assert not username_validation(username)
 
 
-def test_strings_username_validation_truthy():
-    username = "u__hello_world123"
-    assert username_validation(username)
+def test_strings_username_validation_falsy_2():
+    username = "oooooooooooooooooo________a"
+    assert not username_validation(username)
+
+
+def test_strings_username_validation_falsy_3():
+    username = "a______b_________555555555555aaaa"
+    assert not username_validation(username)
