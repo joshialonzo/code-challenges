a5ac5c0975d888e96845ca498da7ee40dc8f4de5
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-05-05 19:00:31 -0600

    Copilot: Developing with Copilot

--- code_challenges/copilot/expense_calculator/expenses/admin.py
+++ code_challenges/copilot/expense_calculator/expenses/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from .models import Category, Expense
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Category admin"""
+    list_display = ("name",)
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    """Expense admin"""
+    list_display = ("title", "amount", "timestamp", "category")
+    list_filter = ("timestamp", "category")
+    search_fields = ("title", "description")
--- code_challenges/copilot/expense_calculator/expenses/models.py
+++ code_challenges/copilot/expense_calculator/expenses/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    """Category model"""
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Expense(models.Model):
+    """Expense model"""
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
