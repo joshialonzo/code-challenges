be3e86aef3e5db5167acbda4f74d98c55d29c803
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-05-17 20:50:28 -0600

    Design Patterns: OPP

--- code_challenges/patterns/__init__.py
+++ code_challenges/patterns/__init__.py
@@ -25,4 +25,24 @@ Types of design patterns:
     * Accomplish both functional and non-functional goals.
     * The focus is defining protocols of communication between objects.
     * OPP: Methods.
+
+OPP: Object Oriented Programming
+
+* Objects:
+    * Represents entities in both problem and solution domains.
+        * Problem domain: Concept of the real world.
+        * Solution domain: Concept of the software.
+    * Components representing all the moving parts of your system.
+    * Entities interacting with the software.
+
+* Classes:
+    * Templates for creating objects to avoid recreating them each time.
+    * Classes are the blueprints for objects.
+
+* Attributes:
+    * Represents properties of an entity.
+    * Captures the state of an entity.
+
+* Methods:
+    * Represents the behavior of an entity.
 """
