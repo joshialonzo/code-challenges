7aa11f71de1c0b12f2d74c336ef558d6b2724e46
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-05-17 20:29:47 -0600

    Design Patterns: Definition and Types

---
+++ code_challenges/patterns/__init__.py
@@ -0,0 +1,28 @@
+"""
+Design Patterns
+
+Well-known solutions to common problems in software design.
+They are well-accepted solutions by the developer community.
+
+You use design patterns because you don't want to reinvent the wheel.
+
+Types of design patterns:
+
+* Creational:
+    * We use these to create objects systematically.
+    * The main benefit is the flexibility.
+    * It is pretty useful for settings and configurations.
+    * OPP: Polymorphism.
+
+* Structural:
+    * Establish relationships between software components.
+    * Accomplish both functional and non-functional goals.
+    * Different goals require different structures.
+    * OPP: Inheritance.
+
+* Behavioral:
+    * Object interactions.
+    * Accomplish both functional and non-functional goals.
+    * The focus is defining protocols of communication between objects.
+    * OPP: Methods.
+"""
